40d8d652fd005a87d9d90c47b54403c246761d8a
AndresMes <federicoescobar2022@gmail.com>
2025-04-21 22:09:10 -0500

    Mapper de Appointment

---
+++ src/main/java/edu/unimagdalena/consultoriomedico/mappers/AppointmentMapper.java
@@ -0,0 +1,20 @@
+package edu.unimagdalena.consultoriomedico.mappers;
+
+import edu.unimagdalena.consultoriomedico.DTO.request.AppointmentDtoRequest;
+import edu.unimagdalena.consultoriomedico.DTO.response.AppointmentDtoResponse;
+import edu.unimagdalena.consultoriomedico.entities.Appointment;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface AppointmentMapper {
+    @Mapping(source = "patient.idPatient", target = "idPatient")
+    @Mapping(source = "doctor.idDoctor", target = "idDoctor")
+    @Mapping(source = "consultRoom.idConsultRoom", target = "idConsultRoom")
+    AppointmentDtoResponse toDto(Appointment appointment);
+
+    @Mapping(target = "patient", ignore = true)
+    @Mapping(target = "doctor", ignore = true)
+    @Mapping(target = "consultRoom", ignore = true)
+    Appointment toEntity(AppointmentDtoRequest appointment);
+}
