07c8b3b96723afbd7902b48332d4938c7063e420
AndresMes <federicoescobar2022@gmail.com>
2025-04-23 19:07:09 -0500

    Controllers created

---
+++ src/main/java/edu/unimagdalena/consultoriomedico/controllers/AppointmentController.java
@@ -0,0 +1,48 @@
+package edu.unimagdalena.consultoriomedico.controllers;
+
+import edu.unimagdalena.consultoriomedico.DTO.request.AppointmentDtoRequest;
+import edu.unimagdalena.consultoriomedico.DTO.request.AppointmentDtoUpdateRequest;
+import edu.unimagdalena.consultoriomedico.DTO.response.AppointmentDtoResponse;
+import edu.unimagdalena.consultoriomedico.services.AppointmentService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/appointments")
+@RequiredArgsConstructor
+public class AppointmentController {
+
+    private final AppointmentService appointmentService;
+
+    @GetMapping
+    public ResponseEntity<List<AppointmentDtoResponse>> getAllAppointments() {
+        return ResponseEntity.ok(appointmentService.findAllAppointments());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<AppointmentDtoResponse> getAppointmentById(@PathVariable Long id) {
+        return ResponseEntity.ok(appointmentService.findAppointmentById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<AppointmentDtoResponse> createAppointment(@Valid @RequestBody AppointmentDtoRequest appointmentDtoRequest){
+        return ResponseEntity.status(HttpStatus.CREATED).body(appointmentService.saveAppointment(appointmentDtoRequest));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<AppointmentDtoResponse> updateAppointment(@PathVariable Long id, @Valid @RequestBody AppointmentDtoUpdateRequest appointmentDtoRequest){
+        return ResponseEntity.ok(appointmentService.updateAppointment(id, appointmentDtoRequest));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteAppointment(@PathVariable Long id){
+        appointmentService.deleteAppointment(id);
+        return ResponseEntity.noContent().build();
+    }
+
+}
---
+++ src/main/java/edu/unimagdalena/consultoriomedico/controllers/ConsultRoomController.java
@@ -0,0 +1,47 @@
+package edu.unimagdalena.consultoriomedico.controllers;
+
+import edu.unimagdalena.consultoriomedico.DTO.request.ConsultRoomDtoRequest;
+import edu.unimagdalena.consultoriomedico.DTO.response.ConsultRoomDtoResponse;
+import edu.unimagdalena.consultoriomedico.services.ConsultRoomService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/rooms")
+@RequiredArgsConstructor
+public class ConsultRoomController {
+
+    private final ConsultRoomService consultRoomService;
+
+    @GetMapping
+    public ResponseEntity<List<ConsultRoomDtoResponse>> getAllConsultRooms() {
+        return ResponseEntity.ok(consultRoomService.findAllConsultRooms());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ConsultRoomDtoResponse> getConsultRoomById(@PathVariable Long id){
+        return ResponseEntity.ok(consultRoomService.findConsultRoomById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<ConsultRoomDtoResponse> createConsultRoom(@Valid @RequestBody ConsultRoomDtoRequest consultRoomDtoRequest){
+        return ResponseEntity.status(HttpStatus.CREATED).body(consultRoomService.saveConsultRoom(consultRoomDtoRequest));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ConsultRoomDtoResponse> updateConsultRoom(@PathVariable Long id, @Valid @RequestBody ConsultRoomDtoRequest consultRoomDtoRequest){
+        return ResponseEntity.ok(consultRoomService.updateConsultRoom(id, consultRoomDtoRequest));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteConsultRoom(@PathVariable Long id){
+        consultRoomService.deleteConsultRoom(id);
+        return ResponseEntity.noContent().build();
+    }
+
+}
